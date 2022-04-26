aa7cdfd94bb72c0b72434cf0723d16803986c750
unknown <micaela.rojas@tecsup.edu.pe>
2022-04-26 00:02:13 -0500

    first commit

---
+++ semana2-api-main/src/components/user/controller.js
@@ -0,0 +1,76 @@
+import { prisma } from "../../db";
+
+export const index = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+
+    return res.status(200).json({
+      ok: true,
+      data: users,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      data: error.message,
+    });
+  }
+};
+
+export const store = async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: { ...req.body },
+    });
+
+    return res.status(201).json({
+      ok: true,
+      data: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      data: error.message,
+    });
+  }
+};
+
+export const upsert = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await prisma.user.update({
+      where: { id: Number(id) },
+      data: { ...req.body },
+    });
+
+    return res.status(200).json({
+      ok: true,
+      data: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      data: error.message,
+    });
+  }
+};
+
+export const destroy = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await prisma.user.delete({
+      where: { id: Number(id) },
+    });
+
+    return res.status(200).json({
+      ok: true,
+      data: "User deleted",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      data: error.message,
+    });
+  }
+};
